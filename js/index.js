1d3c523f2d5959743615594577273681f58ff92c
Stefani <steramos76@gmail.com>
2025-01-13 21:04:45 -0300

    iniciando carrinho

--- js/index.js
+++ js/index.js
@@ -5,12 +5,15 @@
 
 const produtosContainer = document.querySelector('#innerProdutos')
 
+let produtos = []
 
 $.ajax({
     url: "https://jezdev.com.br/estudo_ste/",
     method: "GET",
     success: function (result) {
         console.log(result)
+
+        produtos = result.message.data
 
         for (produto of result.message.data) {
             const col = document.createElement('div')
@@ -22,8 +25,9 @@ $.ajax({
                         <p class="card-text">${produto.titulo} - ${produto.descricao}</p>
                         <button onclick="cliquei(this.parentElement)" class="btn btn-success w-100 botaoValor">${produto.valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</button>
                         <div class="btn-group visually-hidden w-100" role="group" aria-label="Basic example">
-                            <button type="button" class="btn btn-primary"><i class="bi bi-dash-lg w-50"></i></button>
-                            <button type="button" class="btn btn-primary"><i class="bi bi-plus-lg w-50"></i></button>
+                            <button type="button" onclick="quantidade(this, -1, ${produto.id})" class="btn btn-primary"><i class="bi bi-dash-lg w-50"></i></button>
+                            <div id="quantidade" class="px-3 d-flex align-items-center">5</div>
+                            <button type="button" onclick="quantidade(this, 1, ${produto.id})" class="btn btn-primary"><i class="bi bi-plus-lg w-50"></i></button>
                         </div>
                     </div>
                 </div>
@@ -41,4 +45,49 @@ function cliquei(cardbody) {
     botaoValor.classList.add('visually-hidden')
 }
 
+function quantidade(elemento, quantidade, id) {
+    console.log(elemento, quantidade, id)
 
+    let produto;
+
+    for (p of produtos) {
+        if (id == p.id) {
+            produto = p
+            break
+        }
+    }
+
+    let carrinho = localStorage.getItem("carrinho")
+    if (carrinho) {
+        carrinho = JSON.parse(carrinho)
+        console.log(carrinho)
+
+        let produtoEncontrado = false
+
+        for (c of carrinho) {
+            if (id == c.id) {
+                c.quantidadeCarrinho += quantidade
+                produtoEncontrado = true
+                break
+            }
+        }
+
+        if (produtoEncontrado == false) {
+            produto.quantidadeCarrinho = 1
+            carrinho.push(produto)
+        }
+
+        let arrayProdutos = JSON.stringify(carrinho)
+        localStorage.setItem("carrinho", arrayProdutos)
+    } 
+    else {
+        produto.quantidadeCarrinho = 1
+        let arrayProdutos = JSON.stringify([produto])
+        console.log(arrayProdutos)
+        localStorage.setItem("carrinho", arrayProdutos)
+    }
+
+
+    //     localStorage.setItem("carrinho", carrinho);
+    //     localStorage.getItem("lastname");
+}
